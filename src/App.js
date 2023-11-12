1d3e01d221860907f2a0621c581a09405466d54b
Rasmus <pr1nce@live.dk>
2023-11-12 17:23:43 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -5,128 +5,130 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startTime: null,
-      currentTime: null,
-      distance: 0,
+      runData: [],
+      timer: 0,
       isRunning: false,
       isPaused: false,
-      lastPosition: null,
+      distance: 0,
     };
   }
 
   componentDidMount() {
-    // Start watching the position when the component mounts
-    this.watchPosition();
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
   }
 
-  watchPosition() {
+  startRun = () => {
+    this.setState({
+      isRunning: true,
+      isPaused: false,
+      runData: [],
+      timer: 0,
+      distance: 0,
+    });
+    this.getUserLocation();
+  };
+
+  pauseRun = () => {
+    this.setState({ isPaused: true });
+  };
+
+  resumeRun = () => {
+    this.setState({ isPaused: false });
+    this.getUserLocation();
+  };
+
+  endRun = () => {
+    this.setState({ isRunning: false, isPaused: false });
+    this.calculateDistance();
+  };
+
+  calculateDistance = () => {
+    const { runData } = this.state;
+
+    if (runData.length < 2) {
+      return;
+    }
+
+    let totalDistance = 0;
+
+    for (let i = 1; i < runData.length; i++) {
+      const prevLocation = runData[i - 1];
+      const currentLocation = runData[i];
+
+      const lat1 = prevLocation.latitude;
+      const lon1 = prevLocation.longitude;
+      const lat2 = currentLocation.latitude;
+      const lon2 = currentLocation.longitude;
+
+      const R = 6371e3; // Earth radius in meters
+      const φ1 = (lat1 * Math.PI) / 180;
+      const φ2 = (lat2 * Math.PI) / 180;
+      const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+      const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+      const a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+      const distance = R * c;
+
+      totalDistance += distance;
+    }
+
+    // Convert distance to centimeters (1 meter = 100 centimeters)
+    const distanceInCentimeters = totalDistance * 100;
+
+    this.setState({ distance: distanceInCentimeters });
+    console.log("Distance: ", distanceInCentimeters);
+  };
+
+  getUserLocation = () => {
     if (navigator.geolocation) {
-      this.positionWatcher = navigator.geolocation.watchPosition(
+      navigator.geolocation.getCurrentPosition(
         (position) => {
-          // Calculate distance if the run is active
+          const { latitude, longitude } = position.coords;
+          this.setState((prevState) => ({
+            runData: [...prevState.runData, { latitude, longitude }],
+          }));
+
           if (this.state.isRunning && !this.state.isPaused) {
-            const distance = this.calculateDistance(
-              this.state.lastPosition,
-              position.coords
-            );
-            this.setState((prevState) => ({
-              distance: prevState.distance + distance,
-              lastPosition: position.coords,
-            }));
+            setTimeout(this.getUserLocation, 1000);
           }
         },
         (error) => {
-          console.error("Error getting location:", error);
-        },
-        { enableHighAccuracy: true }
+          console.log("Error getting location: ", error);
+        }
       );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
     }
-  }
-
-  calculateDistance(coord1, coord2) {
-    // Simple calculation for demonstration purposes
-    // You may want to use a more accurate formula for your needs
-    const lat1 = coord1.latitude;
-    const lon1 = coord1.longitude;
-    const lat2 = coord2.latitude;
-    const lon2 = coord2.longitude;
-
-    const R = 6371; // Radius of the Earth in kilometers
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * (Math.PI / 180)) *
-        Math.cos(lat2 * (Math.PI / 180)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c;
-
-    return distance;
-  }
-
-  handleStartRun = () => {
-    this.setState({
-      startTime: new Date(),
-      currentTime: new Date(),
-      isRunning: true,
-      isPaused: false,
-    });
-  };
-
-  handlePauseRun = () => {
-    this.setState({
-      isPaused: true,
-    });
-  };
-
-  handleResumeRun = () => {
-    this.setState({
-      isPaused: false,
-    });
-  };
-
-  handleEndRun = () => {
-    navigator.geolocation.clearWatch(this.positionWatcher);
-    this.setState({
-      isRunning: false,
-      isPaused: false,
-      lastPosition: null,
-    });
   };
 
   render() {
-    const { startTime, currentTime, distance, isRunning, isPaused } =
-      this.state;
+    const { isRunning, isPaused, distance, timer } = this.state;
 
     return (
       <div>
-        <h4>Running App</h4>
-        <button onClick={this.handleStartRun} disabled={isRunning}>
-          Start Run
-        </button>
-        <button onClick={this.handlePauseRun} disabled={!isRunning || isPaused}>
-          Pause Run
-        </button>
-        <button onClick={this.handleResumeRun} disabled={!isRunning || !isPaused}>
-          Resume Run
-        </button>
-        <button onClick={this.handleEndRun} disabled={!isRunning}>
-          End Run
-        </button>
-        <div>
-          <p>Start Time: {startTime && startTime.toLocaleTimeString()}</p>
-          <p>Current Time: {currentTime && currentTime.toLocaleTimeString()}</p>
-          <p>Distance: {distance.toFixed(2)} km</p>
-        </div>
+        <h4>Using geolocation JavaScript API in React</h4>
+        {isRunning && <h2>Time: {timer} seconds</h2>}
+        {isRunning && !isPaused && (
+          <h2>Distance: {distance.toFixed(2)} centimeters</h2>
+        )}
+        {!isRunning ? (
+          <button onClick={this.startRun}>Start Run</button>
+        ) : (
+          <>
+            <button onClick={this.pauseRun}>Pause</button>
+            <button onClick={this.resumeRun}>Resume</button>
+            <button onClick={this.endRun}>End Run</button>
+          </>
+        )}
       </div>
     );
   }
 }
-
 export default App;
-
 render(<App />, document.getElementById("root"));
